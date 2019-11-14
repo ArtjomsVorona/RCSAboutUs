00738f6f26b8612da778b49585fe02f0f356f863
Artjoms Vorona <artjomsvorona@gmail.com>
2019-11-14 22:20:50 +0200

    Storyboard updates, info tab added, appicon, webviewController.

---
+++ RCSAboutUs/Controllers/WebViewController.swift
@@ -0,0 +1,21 @@
+//
+//  WebViewController.swift
+//  RCSAboutUs
+//
+//  Created by Artjoms Vorona on 14/11/2019.
+//  Copyright © 2019 Artjoms Vorona. All rights reserved.
+//
+
+import WebKit
+import UIKit
+
+class WebViewController: UIViewController {
+    
+    @IBOutlet weak var gitWebView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
